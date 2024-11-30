0ddab21889b42c5d3c8b127ae9c0c445631f5eab
tobiasjongen <Tobias.Jongen@student.hpi.de>
2024-11-30 13:39:18 +0100

    init newsfeed backend

---
+++ newsfeed/backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+'''CORS policy'''
+# Define the origins that should be allowed to make CORS requests
+origins = [
+    #TBD
+]
+
+# Add CORS middleware to FastAPI app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
