76649dd66c08c4314d2e8740c1a82986b76e7794
Tobias Jongen <Tobias.Jongen@student.hpi.de>
2024-12-16 11:20:20 +0100

    improve layout

--- newsfeed/frontend-python/main.py
+++ newsfeed/frontend-python/main.py
@@ -1,7 +1,6 @@
 from nicegui import ui
 import requests
 
-
 localNews : dict = requests.get("http://localhost:8000/local").json()
 schoolNews : dict = requests.get("http://localhost:8000/school").json()
 
@@ -12,24 +11,23 @@ def renderList(newsJson : dict):
     for news in newsJson:
         with ui.item(on_click=lambda link=news["link"]: readMore(link)):
             with ui.item_section():
-                ui.item_label(news["title"])
-                ui.item_label(news["teaser"]).props('caption')
-            with ui.item_section().props('side'):
-                ui.icon('read_more')
+                ui.item_label(news["title"]).classes('text-lg')
+                if news["teaser"] != "":
+                    ui.item_label(news["teaser"]).props('caption').classes('text-base')
 
 
 with ui.header(elevated=True).style('background-color: #3874c8').classes('items-center justify-between'):
-    ui.label('Newsfeed')
+    ui.label('Newsfeed').classes('text-bold, text-xl')
 
 with ui.row().style('display: flex; flex-direction: row;'):
     with ui.column().style('flex: 1; margin-left: 10px;'):
         with ui.list().props('bordered separator'):
-            ui.item_label('Neuigkeiten aus der Schule').props('header').classes('text-bold')
+            ui.item_label('Aktuelles aus der Schule').props('header').classes('text-bold, text-xl')
             ui.separator()
             renderList(schoolNews)
     with ui.column().style('flex: 1; margin-left: 10px;'):
         with ui.list().props('bordered separator'):
-            ui.item_label('Lokale Neuigkeiten').props('header').classes('text-bold')
+            ui.item_label('Lokale Nachrichten').props('header').classes('text-bold, text-xl')
             ui.separator()
             renderList(localNews)
 
