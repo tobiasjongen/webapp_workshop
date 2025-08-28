4a1e8873d813acdc98da35450c79d22fabe2fc15
tobiasjongen <Tobias.Jongen@student.hpi.de>
2025-08-28 14:16:30 +0200

    feat: add highscore endpoints

--- quiz/backend/main.py
+++ quiz/backend/main.py
@@ -1,9 +1,11 @@
 import uvicorn
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, HTTPException
 import json
 import random
 from datetime import datetime
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 import os
 
 app = FastAPI()
@@ -31,6 +33,11 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 with open(dir_path + '/questions.json', 'r') as file:
     questions = json.load(file)
 
+highscores = dict()
+highestUsrId = -1
+
+class Highscore(BaseModel):
+    score: int
 
 @app.get("/")
 async def root():
@@ -41,20 +48,48 @@ async def randomQuestion():
     randInt = random.randint(0, len(questions)-1)
     return questions[randInt]
 
-@app.get("/random_question/{difficulty}", status_code=200)
-async def questionByDifficulty(difficulty, resp: Response):
+@app.get("/random_question/{difficulty}")
+async def questionByDifficulty(difficulty):
     possibleQuestions = []
     for q in questions:
         if q['difficulty'] == int(difficulty):
             possibleQuestions.append(q)
 
     if len(possibleQuestions) == 0:
-        resp.status_code = 500
-        return {"error": f"no questions with difficulty {difficulty} found"}
-    
+        raise HTTPException(
+            status_code=404, 
+            detail=f"no questions with difficulty {difficulty} found"
+        )
     randInt = random.randint(0, len(possibleQuestions)-1)
     return possibleQuestions[randInt]
 
+@app.get("/highscore/{usr_id}")
+async def getUsrHighscore(usr_id: int):
+    global highestUsrId, highscores
+    if usr_id > highestUsrId or usr_id < 0:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"user_id": usr_id, "score": highscores[usr_id]}
+
+@app.get("/highscore")
+async def getAllHighscores():
+    global highscores
+    return highscores
+
+@app.post("/highscore")
+async def storeUsrHigscore(score: Highscore):
+    global highestUsrId, highscores
+    highestUsrId = highestUsrId + 1
+    usr_id = highestUsrId    
+    highscores[usr_id] = score.score
+    return {"user_id": usr_id, "score": score.score}
+
+@app.put("/highscore/{usr_id}")
+async def updateUsrHighscore(usr_id: int, score: Highscore):
+    global highestUsrId, highscores
+    if usr_id > highestUsrId or usr_id < 0:
+        raise HTTPException(status_code=404, detail="User not found")
+    highscores[usr_id] = score.score
+    return {"user_id": usr_id, "score": score.score}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
